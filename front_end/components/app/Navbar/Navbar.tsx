aa5ad96e53a93311ff5ff1bce1c4930b714cffe1
Albat93 <albat93120@gmail.com>
2025-07-06 18:20:47 +0200

    fix: update link in Navbar component to point to the correct author profile path

--- front_end/components/app/Navbar/Navbar.tsx
+++ front_end/components/app/Navbar/Navbar.tsx
@@ -153,7 +153,7 @@ export function Navbar() {
                             </div>
                             <DropdownMenuSeparator />
                             <DropdownMenuItem asChild>
-                                <Link href="/user" className="flex items-center">
+                                <Link href="/author/" className="flex items-center">
                                     <UserIcon className="mr-2 h-4 w-4" />
                                     Mon Profil
                                 </Link>
